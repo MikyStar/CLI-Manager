5dd37e1b5d53d69d4f34d52325fd7e6e1c52b691
AUBENAS Mike <mike.aubenas@gmail.com>
2020-11-25 00:21:53 +0100

    fix

--- src/core/Printer.ts
+++ src/core/Printer.ts
@@ -16,7 +16,7 @@ export namespace Printer
 
 		board.tasks.forEach( task =>
 		{
-			const result = Printer.stringifyTask( task )
+			const result = Task.stringifyTask( task )
 
 			toReturn = [ ...toReturn, ...result ]
 		})
@@ -44,40 +44,6 @@ export namespace Printer
 		});
 
 		return toReturn
-	}
-
-	export const stringifyTask = ( task : ITask, indentLevel : number = 1 ) =>
-	{
-		let toReturn : string[] = []
-		let indent = ''
-		let currentTask = ''
-
-		const isFinalState = task.state === config.states[ config.states.length - 1 ].name
-
-		const stateColor = config.states.filter( state => task.state === state.name )[0].hexColor
-
-		for( let i = 0; i < indentLevel; i++ )
-			indent += '  '
-
-		currentTask +=  chalk.hex( stateColor )( isFinalState ? '☒' : '☐' )
-
-		currentTask += ' '
-		currentTask += isFinalState ? chalk.strikethrough.grey( task.name ) : task.name
-		toReturn.push( indent + currentTask )
-
-		if( !task.subtasks || task.subtasks.length === 0 )
-			return toReturn
-		else
-		{
-			task.subtasks.forEach( sub =>
-			{
-				const result = Printer.stringifyTask( sub, indentLevel + 1 )
-
-				toReturn = [ ...toReturn, ...result ]
-			});
-
-			return toReturn
-		}
 	}
 
 	////////////////////////////////////////
