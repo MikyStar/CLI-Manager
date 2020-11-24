43c306aca25a76c0f0131cc3f77b4cd28fb3544f
AUBENAS Mike <mike.aubenas@gmail.com>
2020-11-25 00:20:11 +0100

    refactor: stringify in task

--- src/core/Task.ts
+++ src/core/Task.ts
@@ -1,4 +1,8 @@
+import chalk from 'chalk'
+
 import { Board } from './ConfigFile'
+import { config } from './ConfigFile'
+import { Printer } from './Printer'
 
 ////////////////////////////////////////
 
@@ -62,4 +66,39 @@ export namespace Task
 
 		return toReturn
 	}
+
+	export const stringifyTask = ( task : ITask, indentLevel : number = 1 ) =>
+	{
+		let toReturn : string[] = []
+		let indent = ''
+		let currentTask = ''
+
+		const isFinalState = task.state === config.states[ config.states.length - 1 ].name
+
+		const stateColor = config.states.filter( state => task.state === state.name )[0].hexColor
+
+		for( let i = 0; i < indentLevel; i++ )
+			indent += '  '
+
+		currentTask +=  chalk.hex( stateColor )( isFinalState ? '☒' : '☐' )
+
+		currentTask += ' '
+		currentTask += isFinalState ? chalk.strikethrough.grey( task.name ) : task.name
+		toReturn.push( indent + currentTask )
+
+		if( !task.subtasks || task.subtasks.length === 0 )
+			return toReturn
+		else
+		{
+			task.subtasks.forEach( sub =>
+			{
+				const result = Task.stringifyTask( sub, indentLevel + 1 )
+
+				toReturn = [ ...toReturn, ...result ]
+			});
+
+			return toReturn
+		}
+	}
+
 }
