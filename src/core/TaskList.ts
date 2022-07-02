a2fe9718f059a4db2a3157a9a9ad644ef90f9681
AUBENAS Mike <mike.aubenas@gmail.com>
2022-07-02 12:08:42 +0200

    fix(stats): no NaN if empty storage

--- src/core/TaskList.ts
+++ src/core/TaskList.ts
@@ -315,7 +315,8 @@ export class TaskList extends Array<Task>
 		states.forEach( ( state, index ) =>
 		{
 			const count = this.search( 'state', state.name ).length
-			const percent = ( count / totalCount ) * 100
+			const _percent = ( count / totalCount ) * 100
+			const percent = isNaN(_percent) ? 0 : _percent
 
 			if( ( index !== 0 ) && ( index !== states.length ) )
 				toReturn += ' ► '
